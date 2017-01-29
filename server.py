f19e74dab91bf0d79ed3fe295e2aabc7f93c85f4
Mark Griffith <mgriffit@ualberta.ca>
2017-01-29 06:01:10 -0700

    reworked the server

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
-# from arthur import Arthur
+from arthur import Arthur
 import phrases
 import requests
-# arthur = Arthur()
+import json
+arthur = Arthur()
 
 app = Flask(__name__)
 
@@ -17,18 +18,62 @@ def reply(user_id, msg):
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
 
+# @app.route('/', methods=['POST'])
+# def handle_incoming_messages():
+#     data = request.json
+#     sender = data['entry'][0]['messaging'][0]['sender']['id']
+#     message = data['entry'][0]['messaging'][0]['message']['text']
+#     if ACTION_WORD == None:
+#         reply_msg, ACTION_WORD = arthur.classify_input(message) # returns None if not action prompt
+#         if ACTION_WORD == None:
+#             reply_msg = phrases.determine_response(message)
+#     else:
+#         reply_msg = arthur.action(ACTION_WORD,message)
+#     reply(sender, reply_msg)
+#     return 'ok'
+
+
+
+def messaging_events(payload):
+    """Generate tuples of (sender_id, message_text) from the
+    provided payload.
+    """
+    data = json.loads(payload)
+    messaging_events = data["entry"][0]["messaging"]
+    for event in messaging_events:
+        if "message" in event and "text" in event["message"]:
+            yield event["sender"]["id"], event["message"]["text"]
+
+def send_message(recipient, text):
+    """Send the message text to recipient with id recipient.
+    """
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages",
+        params={"access_token": ACCESS_TOKEN},
+        data=json.dumps({
+            "recipient": {"id": recipient},
+            "message": {"text": text}
+        }),
+        headers={'Content-type': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        print r.text
+
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
-    data = request.json
-    sender = data['entry'][0]['messaging'][0]['sender']['id']
-    message = data['entry'][0]['messaging'][0]['message']['text']
-    if ACTION_WORD == None:
-        reply_msg, ACTION_WORD = arthur.classify_input(message) # returns None if not action prompt
-        if ACTION_WORD == None:
-            reply_msg = phrases.determine_response(message)
-    else:
-        reply_msg = arthur.action(ACTION_WORD)
-    reply(sender, reply_msg)
+    payload = request.get_data()
+    for sender, message in messaging_events(payload):
+        print "Incoming from %s: %s" % (sender, message)
+
+        # if ACTION_WORD == None:
+        #     response, ACTION_WORD = arthur.classify_input(message) # returns None if not action prompt
+        #     if ACTION_WORD == None:
+        #         response = phrases.determine_response(message)
+        # else:
+        #     response = arthur.action(ACTION_WORD,message)
+
+        response = phrases.determine_response(message)
+
+        print "Outgoing to %s: %s" % (sender, response)
+        send_message(sender, response)
     return "ok"
 
 @app.route('/', methods=['GET'])
@@ -44,3 +89,4 @@ def handle_verification():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    ACTION_WORD = None
