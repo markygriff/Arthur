5bca4ddf5a02793015cdc044ca3c2531c024efa5
Mark Griffith <mgriffit@ualberta.ca>
2017-01-29 01:07:40 -0700

    test server working
    Arthur can reply to messages

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import requests
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = "EAACZBlPXesb0BAK56P4eeDHlXbZB84c8IwZCGDZC8RkJlJrNJ6zyG97UiMoa4fpZCeVo0QsPNJkOZBaJZBGBChyHRVQawzZC3iR9qWcZBXJpvYCIZCDFBgoUqJUX2ZCnPVGo4MFi5P2RooT3Ad7HMTqj1KZBblQZAlCTyIDTTyQIXmdHOeAZDZD"
+ACCESS_TOKEN = "EAACZBlPXesb0BALRYMeooVhYbScAAJFXd3ZBAJHxZCrD3ZAK5sGyrkeD8WnI3Mck5fr98AHryCJC8lBE9VfQqTWsU9oPSqQpeNxrDhlXTd4Br4ewyzOfLXB3e03eRrzFvSuA6pyFVTw6AukSjh4a5Jfe8XrhlAYXxFGrHnoZAiwZDZD"
 
 
 def reply(user_id, msg):
@@ -18,16 +18,24 @@ def reply(user_id, msg):
     resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
 
-
 @app.route('/', methods=['POST'])
 def handle_incoming_messages():
     data = request.json
     sender = data['entry'][0]['messaging'][0]['sender']['id']
     message = data['entry'][0]['messaging'][0]['message']['text']
     reply(sender, message[::-1])
-
     return "ok"
 
+@app.route('/', methods=['GET'])
+def handle_verification():
+    print "Handling Verification."
+    if request.args.get('hub.verify_token', '') == 'my_voice_is_my_password_verify_me':
+    # if request.args.get('hub.verify_token', '') == 'secret':
+        print "Verification successful!"
+        return request.args.get('hub.challenge', '')
+    else:
+        print "Verification failed!"
+        return 'Error, wrong validation token'
 
 if __name__ == '__main__':
     app.run(debug=True)
