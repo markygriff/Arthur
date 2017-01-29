3b0377dec00a4afba79e55b5781ccbddd514e1fe
Jordan Lane <jordanbl2007@hotmail.com>
2017-01-29 03:16:38 -0700

    Properly configured news API

--- topnews_controller.py
+++ topnews_controller.py
@@ -1,21 +1,28 @@
-'''Returns info on top news stories
-        Powered by News API
-
-        News API Key: d1159719ec474bf1b82575b5b8478bef
-        Default GET: https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey={API_KEY}
-        API has two options: source, sortBy [to change these options you can modify the GET URL]
-
-        pip install urllib2
-        pip install json
-        '''
+#pip install urllib2
+#pip install json
 
 import urllib2
 import json
 
 def get_top_news():
+    '''Returns list of top news stories. Powered by News API
+
+        Currently returns a list of the top news headlines.
+        Possible TODO: allow user to 
+
+        News API Key: d1159719ec474bf1b82575b5b8478bef
+        Default GET: https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey={API_KEY}
+        API has two options: source, sortBy [to change these options you can modify the GET URL]
+
+    '''
     articles = []
     response = urllib2.urlopen("https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=d1159719ec474bf1b82575b5b8478bef")
     data = json.load(response)
 
     for i in range(len(data["articles"])):
         articles.append(data["articles"][i]["title"])
+
+    return articles
+
+if __name__ == '__main__':
+    print(get_top_news())
