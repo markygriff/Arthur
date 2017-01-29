02186fe0961134ae09367db8231ffdb680cafa0c
Jordan Lane <jordanbl2007@hotmail.com>
2017-01-28 19:22:28 -0700

    Interact with News API

--- topnews_fn.py
+++ topnews_fn.py
@@ -1,1 +1,13 @@
 #Returns info on top news stories
+# Powered by News API
+
+# News API Key: d1159719ec474bf1b82575b5b8478bef
+# Default GET: https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey={API_KEY}
+import urllib2
+import json
+
+response = urllib2.urlopen("https://newsapi.org/v1/articles?source=cnn&sortBy=top&apiKey=d1159719ec474bf1b82575b5b8478bef")
+data = json.load(response)
+
+for i in range(len(data["articles"])):
+    print data["articles"][i]["title"]
