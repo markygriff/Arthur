eb119b917978be87824a54e87c94fdec329b5889
Jordan Lane <jordanbl2007@hotmail.com>
2017-01-28 19:31:11 -0700

    some comments

--- topnews_fn.py
+++ topnews_fn.py
@@ -1,8 +1,11 @@
-#Returns info on top news stories
-# Powered by News API
+'''Returns info on top news stories
+        Powered by News API
 
-# News API Key: d1159719ec474bf1b82575b5b8478bef
-# Default GET: https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey={API_KEY}
+        News API Key: d1159719ec474bf1b82575b5b8478bef
+        Default GET: https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey={API_KEY}
+        API has two options: source, sortBy [to change these options you can modify the GET URL]
+        '''
+        
 import urllib2
 import json
 
